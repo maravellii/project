2dfb98a7bbd8271a2d463d8fdab366cf33a430fd
Celicja <lxqd@wp.pl>
2026-03-06 19:16:21 +0100

    Poprawki w docker-compose

---
+++ airflow/dags/ecommerce_pipeline.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1),
+    "retries": 1,
+}
+
+with DAG(
+    dag_id="ecommerce_pipeline",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+)   as dag:
+
+    ingestion = BashOperator(
+        task_id="run_ingestion",
+        bash_command="docker exec ecommerce_ingestion python /app/app/load_data.py",
+    )
+
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command="docker exec ecommerce_dbt dbt run",
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command="docker exec ecommerce_dbt dbt test",
+    )
+
+    ingestion >> dbt_run >> dbt_test
